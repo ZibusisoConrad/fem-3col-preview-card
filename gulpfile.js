4e50b8a85627a7a92d241b76e09bbfbec4237567
Zibusiso Conrad <zibusisoconradm@gmail.com>
2021-08-31 18:54:01 +0200

    add scss file and gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+// initialize modules 
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const browsersync = require('browser-sync').create();
+
+// Sass task
+function scssTask(){
+  return src('app/scss/style.scss', {sourcemaps: true})
+  .pipe(sass())
+  .pipe(postcss([autoprefixer()])) // , cssnano()  add this later
+  .pipe(dest('dist', {sourcemap: '.'}));
+}
+
+// Browsersync 
+function browserSyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  cb();
+}
+
+ function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task 
+function watchTask(){
+  watch('*.html', browserSyncReload);
+  watch(
+    ['app/scss/**/*.scss'], 
+    series(scssTask, browserSyncReload)
+  );
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, browserSyncServe, watchTask);
